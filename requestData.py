7b1d0b5c3daf3609b3ef733d672b69774a9cac3a
nilsgaebel <nils.gaebel@gmx.de>
2022-06-26 12:56:56 +0200

    Initial commit
     Changes to be committed:
    	new file:   requestData.py

---
+++ requestData.py
@@ -0,0 +1,11 @@
+import requests
+import json
+headers = {'X-Auth-Token': 'TOKEN'} # token has to be replaced
+r = requests.get("https://api.football-data.org/v4/competitions/CL/matches",headers=headers)
+
+print('Status response of the request: ', r.status_code)
+
+json = json.loads(r.text)
+for t in json['matches']:
+    print(t['homeTeam'])  
+
