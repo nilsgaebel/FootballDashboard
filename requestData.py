41781fa34fb146e03e1b7f4e21c2407913e01397
nilsgaebel <nils.gaebel@gmx.de>
2022-06-27 20:29:09 +0200

    Changes to be committed: 	modified:   requestData.py

--- requestData.py
+++ requestData.py
@@ -1,13 +1,19 @@
 import requests
 import json
-headers = {'X-Auth-Token': 'TOKEN'} # token has to be replaced
-r = requests.get("https://api.football-data.org/v4/competitions/CL/matches",headers=headers)
+headers = {'X-Auth-Token': 'Token'}
+r = requests.get("https://api.football-data.org/v4/competitions/CL/teams",headers=headers)
 
 print('Status response of the request: ', r.status_code)
 
 json = json.loads(r.text)
-for t in json['matches']:
-    print(t['homeTeam'])
-    
-print("DONE")
+team_names = []
+for t in json['teams']:
+     team_names.append(t['name'])
 
+print("team names in UCL: ", team_names)
+
+r2 = requests.get("https://api.football-data.org/v4/matches",headers=headers)
+
+print('Status response of the request: ', r2.status_code)
+test = r2.text
+json_2 = json.loads(test)
